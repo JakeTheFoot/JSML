b4e1c7970a55f32cace47b978f3fda59a400a86d
Jake Silberstein <76013728+JakeTheFoot@users.noreply.github.com>
2023-10-07 19:59:53 -0400

    Removed data creation utils

--- JSML/utils/utils.py
+++ JSML/utils/utils.py
@@ -3,44 +3,6 @@ import json
 import cv2
 import os
 
-
-def Load_MNIST_Dataset(dataset, path):
-
-    # Scan all the directories and create a list of labels
-    labels = os.listdir(os.path.join(path, dataset))
-
-    # Create lists for samples and labels
-    X = []
-    y = []
-
-    # For each label folder
-    for label in labels:
-        # And for each image in given folder
-        for file in os.listdir(os.path.join(path, dataset, label)):
-            # Read the image
-            image = cv2.imread(
-                os.path.join(path, dataset, label, file),
-                cv2.IMREAD_UNCHANGED)
-
-            # And append it and a label to the lists
-            X.append(image)
-            y.append(label)
-
-    # Convert the data to proper numpy arrays and return
-    return np.array(X), np.array(y).astype('uint8')
-
-# MNIST dataset (train + test)
-
-
-def Create_Data_MNIST(path):
-
-    # Load both sets separately
-    X, y = Load_MNIST_Dataset('train', path)
-    X_test, y_test = Load_MNIST_Dataset('test', path)
-
-    # And return all the data
-    return X, y, X_test, y_test
-
 # Create filters
 
 
