1963d389a8a1aa6b25229e9f0944445361fd8738
Jake Silberstein <76013728+JakeTheFoot@users.noreply.github.com>
2023-11-10 21:13:54 -0500

    Cleaned up files and added dataDownload utils

--- JSML/utils/__init__.py
+++ JSML/utils/__init__.py
@@ -1,1 +1,2 @@
 from .utils import *
+from .dataDownloads import *
---
+++ JSML/utils/dataDownloads.py
@@ -0,0 +1,21 @@
+from zipfile import ZipFile
+import os
+import urllib
+import urllib.request
+
+# Create a MNIST dataset
+
+def CreateDataMNIST():
+    URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
+    FILE = 'fashion_mnist_images.zip'
+    FOLDER = 'fashion_mnist_images'
+
+    if not os.path.isfile(FILE):
+        print(f'Downloading {URL} and saving as {FILE}...')
+        urllib.request.urlretrieve(URL, FILE)
+
+    print('Unzipping images...')
+    with ZipFile(FILE) as zip_images:
+        zip_images.extractall(FOLDER)
+
+    print('Done!')
